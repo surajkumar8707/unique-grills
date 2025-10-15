7a8adef23e1eb49d5ad0a71594d03572cd94c646
Suraj Verma <suraj@cmptechllp.com>
2025-10-16 00:50:45 +0530

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Symfony\Component\Process\Process;
 |
 */
 
+// Route to perform 'git pull' operation
 Route::get('/git-pull', function () {
     try {
         $process = new Process(['git', 'pull', 'origin', 'main']);
